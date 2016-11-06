fb8fa7acf0a1fa759d883739b55682801e70f11a
nick serebrennikov <non.manifold@gmail.com>
2016-11-06 11:05:11 +0300

    cap FPS by dropping frames

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,12 @@
-const FPS = 70
+const FPS = 70.0
+const FRAME_DURATION = 1000.0 / FPS
 
 export  default class Renderer {
     frame_count = 0
     fps_timer = null
     fps = null
+    updateId = null
+    previousDelta = 0
 
     constructor (viewport) {
         this.viewport = viewport
@@ -12,14 +15,23 @@ export  default class Renderer {
     start = () => {
         this.fps = document.getElementById('fps')
         this.fps_timer = setInterval(this.updateFPS, 1000)
-        window.requestAnimationFrame(this.processFrame)
+        this.updateId = window.requestAnimationFrame(this.processFrame)
         this.processFrame()
     }
 
-    processFrame = () => {
+    processFrame = (currentDelta) => {
+        this.updateId = window.requestAnimationFrame(this.processFrame)
+
+        const delta = currentDelta - this.previousDelta
+
+        if (delta < FRAME_DURATION) {
+            return // drop frame
+        }
+
         this.viewport.render()
         this.frame_count ++
-        window.requestAnimationFrame(this.processFrame)
+
+        this.previousDelta = currentDelta
     }
 
 
